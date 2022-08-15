dcc108c2918891be0251a3f508d9ca889081534d
Jean Pierre Vera <jpierre22391@hotmail.com>
2022-08-15 19:15:56 -0400

    e

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,5 +1,6 @@
 // Crearemos muchos botones en la App, usaremos este componente para todos
 // Recibira un onClick
+// Carpeta UI guardara todos los componentes que sean genericos, los que estaran por todos lados de la pagina
 
 import PropTypes from 'prop-types';
 import './uiStyles'
