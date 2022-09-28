687562aa9948653e01dfd95a27a3eb75c12a25a6
Jean Vera <jpierre22391@hotmail.com>
2022-09-28 18:39:56 -0300

    cambio de nombre para archivo principal, se agrega jsconfig

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import Login from './components/Login/index';
-import CiviConectaAdminPanel from './components/CiviConectaAdminPanel/index';
+import Main from './components/Main';
 import './index.css';
 
 function App() {
   return (
     <div>
-      {/* Solamente oculta 1 y los vas dibujando 1 a 1, yo hago las conexiones despues */}
-      <Login />
-      <CiviConectaAdminPanel />
+      <Main />
     </div>
   );
 }
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,12 +1,12 @@
-import "./login.css";
-import ImageSection from "./ImageSection/ImageSection";
-import LoginSection from "./LoginSection/LoginSection";
+import './login.css';
+import ImageSection from './ImageSection/ImageSection';
+import LoginSection from './LoginSection/LoginSection';
 
 const Login = () => {
   return (
     <div className="login">
-      <ImageSection />
       <LoginSection />
+      <ImageSection />
     </div>
   );
 };
