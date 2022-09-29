1686447d292bf4aa4d6479196793f3bbefd5af39
Jean Vera <jpierre22391@hotmail.com>
2022-09-29 10:54:19 -0300

    feat: se agrega componente para recuperacion de contraseña, se agrega su ruta

--- src/components/Login/LoginSection/LoginSection.js
+++ src/components/Login/LoginSection/LoginSection.js
@@ -1,5 +1,6 @@
 import './loginSection.css';
 import LoginForm from '../LoginForm/LoginForm';
+import { Link } from 'react-router-dom';
 
 const LoginSection = () => {
   return (
@@ -13,7 +14,8 @@ const LoginSection = () => {
       <LoginForm />
       <div className="login-section-footer">
         <div>
-          Problemas al ingresar al sitio? <a href="#'">Click aquí</a>
+          Problemas al ingresar al sitio?{' '}
+          <Link to="../recover-password">Click aqu&iacute;</Link>
         </div>
       </div>
     </div>
