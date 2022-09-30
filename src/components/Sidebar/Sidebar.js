cf04526ef0503055db9949ead8e41c8421caa0f0
Jean Vera <jpierre22391@hotmail.com>
2022-09-30 04:00:29 -0300

    fix: se ajusta componente sidebar optimizando su estructura

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import BoardIcon from '../UI/Icons/Board';
-import HomeIcon from '../UI/Icons/Home';
-import StudentIcon from '../UI/Icons/Student';
-import TeacherIcon from '../UI/Icons/Teacher';
+import home from 'src/components/UI/Icons/home.svg';
+import teacher from 'src/components/UI/Icons/teacher.svg';
+import student from 'src/components/UI/Icons/student.svg';
+import blackboard from 'src/components/UI/Icons/blackboard.svg';
 
 import './Sidebar.css';
+import { Link } from 'react-router-dom';
+
+const NavButton = ({ icon, ariaLabel, path }) => {
+  return (
+    <Link to={path} className="nav-button" type="button" aria-label={ariaLabel}>
+      <img src={icon} alt={ariaLabel} />
+    </Link>
+  );
+};
 
 const Sidebar = () => {
   return (
     <nav className="sidebar">
       <div className="sidebar__content">
-        <HomeIcon />
-        <TeacherIcon />
-        <StudentIcon />
-        <BoardIcon />
+        <NavButton icon={home} ariaLabel="home" path={'/auth/login'} />
+        <NavButton icon={teacher} ariaLabel="teacher" />
+        <NavButton icon={student} ariaLabel="student" />
+        <NavButton icon={blackboard} ariaLabel="blackboard" />
       </div>
     </nav>
   );
