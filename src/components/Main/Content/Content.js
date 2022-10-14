d0e54bbc77dbfe8a82fde9a7ed1d25e5cf734b51
ypezoa <y.pezoa22@gmail.com>
2022-10-14 14:33:57 -0300

    Changes
    feat: se agregan efectos de hover
    feat: se agrega contenedor principal para unidades
    feat: se agrega componente para icono con color de fondo

--- src/components/Main/Content/Content.js
+++ src/components/Main/Content/Content.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from 'src/components/Header/Header';
 import SurveyBox from '../SurveyBox/SureyBox';
+import Units from '../Units/Units';
+
 import './Content.css';
 
 const Content = () => {
   return (
     <main className="main-content">
       <Header />
-      <SurveyBox />
+      <section className="main__section">
+        <SurveyBox />
+        <Units />
+      </section>
     </main>
   );
 };
