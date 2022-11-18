3d3b9f2f551be103f7ce215812b6e6d5a9655e97
ypezoa <y.pezoa22@gmail.com>
2022-11-18 16:44:10 -0300

    Contenido
    se agrega componente para unidades, encuestas, efemerides y situaciones
    se agregan rutas para componentes añadidos
    se crea componente SectionsHeader para header con imagen en secciones

--- src/components/Manager/Manager.js
+++ src/components/Manager/Manager.js
@@ -5,6 +5,7 @@ import StageDetail from './StageDetail/StageDetail';
 
 import background from 'src/assets/images/manager-header.png';
 import './Manager.css';
+import SectionsHeader from '../SectionsHeader/SectionsHeader';
 
 const Manager = () => {
   const [stage, setStage] = useState('manager');
@@ -20,17 +21,10 @@ const Manager = () => {
 
   return (
     <>
-      <header className="manager-header">
-        <img src={background} alt="background" />
-        <div className="header__manager-text">
-          <h1>
-            CIVI <span>admin</span>
-          </h1>
-          {stage === 'manager' && <p>Manager de establecimientos</p>}
-          {stage === 'assignment' && <p>{institutionName}</p>}
-          {stage === 'detail' && <p>{institutionName}</p>}
-        </div>
-      </header>
+      <SectionsHeader image={background} />
+      {/*{stage === 'manager' && <p>Manager de establecimientos</p>}
+      {stage === 'assignment' && <p>{institutionName}</p>}
+      {stage === 'detail' && <p>{institutionName}</p>}*/}
       <main className="manager-content">
         <div className="current-path">
           {/*Aqui debe ir el path para controlar flujo actual*/}
