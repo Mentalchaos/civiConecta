d3ef81f839c839d8eb7487689aff2457a4b5c6d1
ypezoa <y.pezoa22@gmail.com>
2022-11-18 16:44:10 -0300

    Contenido
    se agrega componente para unidades, encuestas, efemerides y situaciones
    se agregan rutas para componentes añadidos
    se crea componente SectionsHeader para header con imagen en secciones

--- src/components/Login/LoginForm/LoginForm.js
+++ src/components/Login/LoginForm/LoginForm.js
@@ -1,18 +1,35 @@
-import "./loginForm.css";
+import { useNavigate } from 'react-router-dom';
+import useForm from 'src/hooks/useForm';
+import './loginForm.css';
 
 const LoginForm = () => {
+  const { values, handleInputChange } = useForm({
+    email: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+
+  const onHandleSubmit = e => {
+    e.preventDefault();
+    const { email, password } = values;
+    if (!email || !password) return;
+    if (email === 'admin' && password === 'admin') navigate('/admin/dashboard');
+  };
+
   return (
     <div className="login-container">
-      <form className="login-form" action="" method="POST">
+      <form className="login-form" onSubmit={onHandleSubmit}>
         <div className="form-group">
           <label className="form-label" htmlFor="username">
-            Usuario
+            Email de usuario
           </label>
           <input
             className="form-input"
-            id="username"
+            onChange={handleInputChange}
+            id="email"
+            name="email"
             type="text"
-            placeholder="nombre de usuario"
+            placeholder="Escriba su correo aquí"
           />
         </div>
 
@@ -22,14 +39,21 @@ const LoginForm = () => {
           </label>
           <input
             className="form-input"
+            onChange={handleInputChange}
             id="password"
+            name="password"
             type="password"
-            placeholder="escriba su contraseña"
+            placeholder="Escriba su contraseña"
           />
         </div>
 
         <div className="form-group">
-          <input className="form-input-submit" type="submit" value="Ingresar" />
+          <input
+            onClick={onHandleSubmit}
+            className="form-input-submit"
+            type="submit"
+            value="Ingresar"
+          />
         </div>
       </form>
     </div>
---
+++ src/components/Situations/Situations.js
@@ -0,0 +1,11 @@
+import './Situations.css';
+
+const Situations = () => {
+  return (
+    <main className="main-content">
+      <h1>contenido</h1>
+    </main>
+  );
+};
+
+export default Situations;
