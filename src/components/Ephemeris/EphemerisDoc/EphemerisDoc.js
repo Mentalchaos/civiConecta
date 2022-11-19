14f44876621ced756c9ef47c28b980c841eeb9c5
ElBugs99 <jpvaldivia99@gmail.com>
2022-11-19 05:00:28 -0300

    oeoe

---
+++ src/components/Ephemeris/EphemerisDoc/EphemerisDoc.js
@@ -0,0 +1,13 @@
+import '../Ephemeris.css';
+
+const EphemerisDoc = () => {
+  return (
+    <div className='ephemeris-doc-container'>
+        <div>Día de Civi conecta</div>
+        <div>1 documentos adjuntados.</div>
+        <div>OA</div>
+    </div>
+  );
+};
+
+export default EphemerisDoc;
