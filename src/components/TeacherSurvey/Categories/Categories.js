82aaed055266fca06dd028382210980981750540
ClauO <claudia.osses.mundaca@gmail.com>
2022-12-06 15:12:46 -0300

    'x'

---
+++ src/components/TeacherSurvey/Categories/Categories.js
@@ -0,0 +1,27 @@
+import relationships from '../../../assets/images/relationships-icon.png';
+import editArrow from '../../../assets/images/edit-arrow.png';
+// import studentsIcon from '../../../assets/images/relationships-red.png';
+import './Categories.css';
+
+const Categories = ( {title, detail, type} ) => {
+
+    const css = {color: type == 'student' ? '#D9687C'  : ''};
+
+    return (
+        <div className="category-container">
+            <div className='category-icon'>
+                <img src={relationships} alt="relationships-con"/>
+            </div>
+            <div className='category-description'>
+                <p style={css} className='category-title'>{title}</p>
+                <p className='category-details'>{detail}</p>
+            </div>
+            <div className='edit-arrow'>
+                <p style={css} className='edit-hover'>Editar categoría</p>
+                <img src={editArrow} alt="arrow-icon"/>
+            </div>
+        </div>
+    )
+}
+
+export default Categories;
--- src/components/TeacherSurvey/TeacherSurvey.js
+++ src/components/TeacherSurvey/TeacherSurvey.js
@@ -1,7 +1,62 @@
+import { useState } from 'react';
+import SectionsHeader from '../SectionsHeader/SectionsHeader';
+import Categories from './Categories/Categories';
+import teacherImage from '../../assets/images/teacher-banner.png';
 import './TeacherSurvey.css';
 
 const TeacherSurvey = () => {
-  return <h1>Teacher Survey work</h1>;
+  const surveyCategories = [
+    {
+      title: 'Relaciones interpersonales',
+      detail: '0 Preguntas en esta categoría'
+    },
+    {
+      title: 'Resolución de conflictos',
+      detail: '0 Preguntas en esta categoría'
+    },
+    {
+      title: 'Bienestar y autocuidado',
+      detail: '0 Preguntas en esta categoría'
+    },
+    {
+      title: 'Prevención consumo drogas y alcohol',
+      detail: '0 Preguntas en esta categoría'
+    }
+  ];
+
+  const [state, setState] = useState(surveyCategories);
+
+  return (
+    <>
+      <SectionsHeader image={teacherImage} />
+      <main className="main-content">
+        <div className="header">
+          <div>
+            <span className="section-title">Encuesta del docente</span>
+          </div>
+        </div>
+        <div className="categories-container">
+          {surveyCategories.map(item => {
+            return (
+              <Categories
+                title={item.title}
+                detail={item.detail}
+                key={item.title}
+              />
+            );
+          })}
+        </div>
+      </main>
+      {state.length < 4 &&
+        <div className='button-container'>
+          <button className='add-button'>
+            <p className='add-button-icon'>+</p>
+            <p className='add-button-text'>Añadir Categoría</p>
+          </button>
+        </div>
+      }
+    </>
+  );
 };
 
 export default TeacherSurvey;
