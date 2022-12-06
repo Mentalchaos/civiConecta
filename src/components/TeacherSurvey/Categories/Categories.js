4e94a5e123ca8548c03a84495d0f923619be5ab4
Mentalchaos <jpierre22391@hotmail.com>
2022-12-06 17:03:39 -0300

    added state to change screen on surveys section

--- src/components/TeacherSurvey/Categories/Categories.js
+++ src/components/TeacherSurvey/Categories/Categories.js
@@ -3,12 +3,11 @@ import editArrow from '../../../assets/images/edit-arrow.png';
 // import studentsIcon from '../../../assets/images/relationships-red.png';
 import './Categories.css';
 
-const Categories = ( {title, detail, type} ) => {
-
+const Categories = ({ title, detail, type, onclick}) => {
     const css = {color: type == 'student' ? '#D9687C'  : ''};
 
     return (
-        <div className="category-container">
+        <div className="category-container" onClick={onclick}>
             <div className='category-icon'>
                 <img src={relationships} alt="relationships-con"/>
             </div>
