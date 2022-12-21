da43efa492d0b7b03523ab684c033ea79fc180bf
ClauO <claudia.osses.mundaca@gmail.com>
2022-12-20 23:50:23 -0300

    planification component added

--- src/components/PublicSection/index.js
+++ src/components/PublicSection/index.js
@@ -2,12 +2,17 @@ import PublicFooter from './Footer/PublicFooter';
 import PublicHeader from './Header/PublicHeader';
 import Welcome from './Welcome/Welcome';
 import './PublicSection.css';
+import PlanificationType from './Planification/PlanificationType';
 
 const PublicSection = () => {
     return (
         <div>
             <PublicHeader />
             <Welcome />
+            <div className='planification-cont'>
+                <PlanificationType />
+                <PlanificationType />
+            </div>
             <PublicFooter />
         </div>
     )
