3b4e0038434bfaf116fd79b9bbb4a0db70f7a0e5
Sebastian Real <seba.real.a@gmail.com>
2022-12-23 18:52:59 -0300

    wip: save inner state using instittuion manager + memento to check the object

---
+++ src/components/Manager/StageAssignment/memento.js
@@ -0,0 +1,26 @@
+import InstitutionManager from './institutionManager.js';
+
+class Memento {
+  constructor(key = 'institutionWD') {
+    this.key = key;
+  }
+
+  isSerialized() {
+    return this.key in sessionStorage;
+  }
+
+  serialize(institution) {
+    sessionStorage.setItem(this.key, JSON.stringify(institution));
+  }
+
+  deserialize() {
+    const obj = JSON.parse(sessionStorage.getItem(this.key))
+    return new InstitutionManager(obj);
+  }
+
+  destroy() {
+    sessionStorage.removeItem(this.key);
+  }
+}
+
+export default Memento;
