3dc0359c4968042764e360320d152db6b913045f
Sebastian Real <seba.real.a@gmail.com>
2022-12-24 20:41:24 -0300

    improvement: replace Establishment API to handle it more easily

--- src/entities/Grade.js
+++ src/entities/Grade.js
@@ -5,26 +5,19 @@ class Grade {
     this.level = data.level;
     this.createdAt = data.createdAt;
     this.updatedAt = data.updatedAt;
-    this.letters = data.letters.map(l => new Letter(l));
+    this.letters = (data.letters ?? []).map(l => new Letter(l));
   }
 
-  static fromJSON({ name, run, grade, letter }) {
-    const entity = new Grade({ level: grade, letters: [] });
-    entity.addStudent({ letter, name, run });
-
-    return entity;
-  }
-
-  addStudent({ letter, name, run }) {
+  addLetter(letter) {
     let entity = this.letters.find(l => l.character === letter);
 
     if (entity) {
-      entity.addStudent({ name, run });
-      return;
+      return entity;
     }
 
-    entity = Letter.fromJSON({ letter, name, run });
+    entity = new Letter({ character: letter });
     this.letters.push(entity);
+    return entity;
   }
 
   toJSON() {
