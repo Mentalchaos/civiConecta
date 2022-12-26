88c5e563b721c2ae9288c083fdd19c64bbf45953
ClauO <claudia.osses.mundaca@gmail.com>
2022-12-26 16:53:27 -0300

    asdasd

--- src/components/PublicSection/index.js
+++ src/components/PublicSection/index.js
@@ -3,12 +3,14 @@ import PublicHeader from './Header/PublicHeader';
 import Welcome from './Welcome/Welcome';
 import './PublicSection.css';
 import PlanificationType from './Planification/PlanificationType';
+import PlanificationText from './PlanificationText/PlanificationText';
 
 const PublicSection = () => {
     return (
         <div>
             <PublicHeader />
             <Welcome />
+            <PlanificationText />
             <div className='planification-cont'>
                 <PlanificationType />
                 <PlanificationType />
