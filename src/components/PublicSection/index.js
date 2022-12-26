08b2c3ca3b1f2f5fab44f3e668e5fc6c415cc156
Jean Vera <jpierre22391@hotmail.com>
2022-12-26 19:13:14 -0300

    units components and styles

--- src/components/PublicSection/index.js
+++ src/components/PublicSection/index.js
@@ -4,6 +4,9 @@ import Welcome from './Welcome/Welcome';
 import './PublicSection.css';
 import PlanificationType from './Planification/PlanificationType';
 import PlanificationText from './PlanificationText/PlanificationText';
+import SurveyLink from './SurveyLink/SurveyLink';
+import UnitsHeader from './Units/UnitsHeader';
+import UnitComponent from './Units/UnitComponent';
 
 const PublicSection = () => {
     return (
@@ -15,6 +18,16 @@ const PublicSection = () => {
                 <PlanificationType />
                 <PlanificationType />
             </div>
+            <div className='units-cont'>
+                <UnitsHeader />
+                <div className='units-components'>
+                    <UnitComponent />
+                    <UnitComponent />
+                    <UnitComponent />
+                    <UnitComponent />
+                </div>
+            </div>
+            <SurveyLink />
             <PublicFooter />
         </div>
     )
