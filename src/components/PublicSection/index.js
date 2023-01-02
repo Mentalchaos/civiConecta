cdf0e23b4a50059b34b2aba03a5aee43fe48e21c
Jean Vera <jpierre22391@hotmail.com>
2023-01-01 21:22:08 -0300

    more css

--- src/components/PublicSection/index.js
+++ src/components/PublicSection/index.js
@@ -9,6 +9,74 @@ import UnitComponent from './Units/UnitComponent';
 import UnitSituations from './Units/UnitSituations';
 import './PublicSection.css';
 
+const planificationData = [
+    {
+        title: 'Reorganiza la planificación de acuerdo con la realidad de tu curso.',
+        textButton: 'Personalizar planificación'
+    },
+    {
+        title: 'Accede a la planificación estandarizada.',
+        textButton: 'Ver planificación estandarizada'
+    },
+    {
+        title: 'Genera el enlace para que tus estudiantes respondan la encuesta.',
+        textButton: 'Generar enlace'
+    },
+    {
+        title: 'Revisa el progreso de la encuesta de tus estudiantes.',
+        textButton: 'Ver progreso'
+    },
+    {
+        title: 'Contesta la encuesta docente.',
+        textButton: 'Ir a la encuesta'
+    }
+]
+
+const mockData = [
+    {
+        status: 'Completada',
+        title: 'Unidad I',
+        subtitle: 'Relaciones interpersonales',
+        description: 'Fomentar trato respetuoso y solidario; rechazar violencia y discriminación en las relaciones.',
+        color: 'unit-green',
+        borderColor: 'border-green'
+    },
+    {
+        status: 'En desarrollo',
+        title: 'Unidad II',
+        subtitle: 'Resolución de conflictos',
+        description: 'Aplicar autónomamente estrategias para la resolución de conflictos.',
+        color: 'unit-purple',
+        borderColor: 'border-purple'
+    },
+    {
+        status: 'Pendiente',
+        title: 'Unidad III',
+        subtitle: 'Bienestar y autocuidado',
+        description: 'Practicar en forma autónoma conductas protectoras y de autocuidado en relación a su cuerpo e intimidad.',
+        color: 'unit-red',
+        borderColor: 'border-red'
+    },
+    {
+        status: 'Pendiente',
+        title: 'Unidad IV',
+        subtitle: 'Autorregulación',
+        description: 'Reconocer y describir causas y consecuencias del consumo de drogas.',
+        color: 'unit-red',
+        borderColor: 'border-red'
+    }
+]
+
+const emergentSituations = [
+    {
+        title: 'Situaciones Emergentes'
+    },
+    {
+        title: 'Efemérides'
+    }
+
+]
+
 const PublicSection = () => {
     return (
         <div>
@@ -16,19 +84,39 @@ const PublicSection = () => {
             <Welcome />
             <PlanificationText />
             <div className='planification-cont'>
-                <PlanificationType />
-                <PlanificationType />
+                {
+                    planificationData.map((data, key) => 
+                        <PlanificationType
+                            title={data.title}
+                            textButton={data.textButton}
+                        />
+                    )
+                }
             </div>
             <div className='units-cont'>
                 <UnitsHeader />
                 <div className='units-components'>
-                    <UnitComponent />
-                    <UnitComponent />
-                    <UnitComponent />
-                    <UnitComponent />
+                    {
+                        mockData.map((data, key) =>
+                            <UnitComponent
+                                key={key}
+                                status={data.status}
+                                title={data.title}
+                                subtitle={data.subtitle}
+                                description={data.description}
+                                color={data.color}
+                                borderColor={data.borderColor}
+                            />
+                        )
+                    }
                     <div className='units-components-two'>
-                        <UnitSituations />
-                        <UnitSituations />
+                        {
+                            emergentSituations.map((data, key) =>
+                                <UnitSituations 
+                                    title={data.title}
+                                />
+                            )
+                        }
                     </div>
                 </div>
             </div>
