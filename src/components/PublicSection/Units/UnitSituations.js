603355d2cd39df6ca6e9ff5ad821bfa5855b0ff7
Jean Vera <jpierre22391@hotmail.com>
2023-01-02 00:08:39 -0300

    share survey styles added

--- src/components/PublicSection/Units/UnitSituations.js
+++ src/components/PublicSection/Units/UnitSituations.js
@@ -1,6 +1,6 @@
 import './UnitSituations.css';
 import emergent from 'src/assets/images/emergent.jpg';
-import goTo from 'src/assets/Icons/go-to-link.svg';
+import goTo from 'src/assets/Icons/open-arrow.svg';
 
 
 const UnitSituations = ({title}) => {
@@ -12,11 +12,11 @@ const UnitSituations = ({title}) => {
                 </div>
                 <div className='unit-subtitle'>
                     <p>Ir a los contenidos</p>
-                    <img src={goTo} />
+                    <img src={goTo} alt="arrow-icon" />
                 </div>
             </div>
             <div className='unit-situations-two'>
-                <img src={emergent} />
+                <img src={emergent} alt="emergent-situation"/>
             </div>
         </div>
     )
