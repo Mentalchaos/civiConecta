389b56f7f2bfc304352bfc58b74b44b3f916bdb5
Sebastian Real <seba.real.a@gmail.com>
2023-02-17 10:10:50 -0300

    added mock data isolated for later use

--- src/config.js
+++ src/config.js
@@ -1,5 +1,17 @@
+const constants = {
+  PlaninicationTypes: {
+    STANDARD: 'standard',
+    CUSTOM: 'custom'
+  },
+  UserTypes: {
+    ADMIN: 'Administrator',
+    USER: 'User'
+  }
+};
+
 const config = {
   baseURL: process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:3001',
+  constants
 };
 
 export default config;
