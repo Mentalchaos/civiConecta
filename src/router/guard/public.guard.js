e32f2ad2b4c876822e2e32ce12469493819db54c
Jean Pierre Vera <jean.vera@opencasa.com>
2023-02-20 12:48:52 -0300

    solved conflicts

--- src/router/guard/public.guard.js
+++ src/router/guard/public.guard.js
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import cookie from 'src/utils/cookie';
+import config from 'src/config';
 
 export const PublicGuard = () => {
   const dataCookies = cookie.getDataParser();
   const { email, role } = dataCookies;
-  if (role === 'Administrator') return <Navigate to={'/admin'} />;
+  if (role === config.constants.UserTypes.ADMIN) return <Navigate to={'/admin'} />;
 
-  return email && role === 'User' ? <Outlet /> : <Navigate to="/" />;
+  return email && role === config.constants.UserTypes.USER ? <Outlet /> : <Navigate to="/" />;
 };
