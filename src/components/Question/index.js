a532d331422fac87ec66c7aa974e7bd7e881f7e7
Sebastian Real <seba.real.a@gmail.com>
2023-02-24 15:09:21 -0300

    refactor teacher survey and refactor teacher|student survey common logic

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,23 +1,30 @@
 import { useParams } from 'react-router-dom';
 import Loading from '../UI/Loading';
 import StudentSurveyLayout from 'src/Layouts/StudentSurveyLayout';
+import TeacherSurveyLayout from 'src/Layouts/TeacherSurveyLayout';
 import Question from './Question';
 import useQuestion from './hooks/useQuestion';
 import { QuestionContext } from './context';
 
-const QuestionSection = () => {
+const generateQuestionSection = (Layout) => () => {
   const { topicId, surveyType } = useParams();
   const { states, setters, actions } = useQuestion(topicId, surveyType);
 
   return (
     <QuestionContext.Provider value={{states, setters, actions}}>
-      <StudentSurveyLayout>
+      <Layout>
         <Loading isLoading={states.isLoading}>
           <Question />
         </Loading>
-      </StudentSurveyLayout>
+      </Layout>
     </QuestionContext.Provider>
   );
 };
 
-export default QuestionSection;
+const StudentQuestionSection = generateQuestionSection(StudentSurveyLayout);
+const TeacherQuestionSection = generateQuestionSection(TeacherSurveyLayout);
+
+export {
+  StudentQuestionSection,
+  TeacherQuestionSection
+};
