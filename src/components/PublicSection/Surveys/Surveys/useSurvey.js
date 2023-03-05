73546c363b706e35c4445a0e5831ed6f93dbee7d
Sebastian Real <seba.real.a@gmail.com>
2023-03-05 12:34:52 -0300

    codigo nuclear de la nasa

---
+++ src/components/PublicSection/Surveys/Surveys/useSurvey.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import * as surveyRequest from 'src/services/admin/surveys.request';
+
+const toArray = (obj) => {
+  return Object.entries(obj).map(e => ({ letter: e[1] }));
+};
+
+const calculatePreviousAnswers = (questions) => {
+  return questions.reduce((answers, question, index) => {
+    if (question.selected) {
+      answers[index] = question.selected;
+    }
+    return answers;
+  }, {});
+};
+
+const useSurvey = (userType) => {
+  const [survey, setSurvey] = useState({});
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [savedAlternatives, setSavedAlternatives] = useState({});
+
+  useEffect(() => {
+    async function fn() {
+      const uuid = '455fd91d-15ac-48b6-8b2a-e75d7891bbab';
+      const response = await surveyRequest.getSurveyToAnswer(userType, uuid);
+      console.log('response', response);
+
+      setSurvey(response.feedback);
+      setQuestions(response.survey);
+      setSavedAlternatives(calculatePreviousAnswers(response.survey));
+    }
+
+    fn();
+  }, []);
+
+  return {
+    setters: {},
+    states: {
+      survey,
+      questions,
+      currentQuestion,
+      savedAlternatives,
+      get isFirstQuestion() {
+        return currentQuestion === 0;
+      },
+      get isLastQuestion() {
+        return currentQuestion === questions.length - 1;
+      },
+      get questionToShow() {
+        return questions[currentQuestion];
+      },
+      get hasQuestions() {
+        return questions.length;
+      }
+    },
+    actions: {
+      goBack() {
+        setCurrentQuestion(currentQuestion - 1);
+      },
+      continue() {
+        if (currentQuestion === questions.length -1) {
+          return alert('sha terminaste la encuesta cheeee');
+        }
+
+        setCurrentQuestion(currentQuestion + 1);
+      },
+      saveAlternative(letter) {
+        return () => {
+          const checkpoint = {
+            ...savedAlternatives,
+            [currentQuestion]: letter
+          };
+          setSavedAlternatives(checkpoint);
+        };
+      },
+      isAlternativeSelected(letter) {
+        return savedAlternatives[currentQuestion] === letter;
+      },
+      sendData() {
+        // const answers = toArray(savedAlternatives);
+      }
+    }
+  };
+};
+
+export default useSurvey;
