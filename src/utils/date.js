85b088e8df8cc202fa3df6d29cbabcb9ac349c76
Mentalchaos <jpierre22391@hotmail.com>
2023-03-12 17:26:37 -0300

    modified planification, and added conditionals to dashboard

---
+++ src/utils/date.js
@@ -0,0 +1,41 @@
+
+const date = {
+  weekdays: [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado'
+  ],
+  months: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ]
+}
+
+const { weekdays, months } = date;
+
+//TODO: improve this fn
+const getFormattedDate = () => {
+  const date = new Date();
+  const weekday = weekdays[date.getDay()];
+  const dayOfMonth = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${weekday} ${dayOfMonth}, ${month} ${year}`;
+};
+
+export { getFormattedDate };
