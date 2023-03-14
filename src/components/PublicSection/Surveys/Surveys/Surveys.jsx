6c3cbce12dfc3b0bba51944e7006af7a7bebb48d
Jean Pierre Vera <jean.vera@opencasa.com>
2023-03-14 17:26:01 -0300

    fixed questions

--- src/components/PublicSection/Surveys/Surveys/Surveys.jsx
+++ src/components/PublicSection/Surveys/Surveys/Surveys.jsx
@@ -18,41 +18,6 @@ const SurveyTypes = {
 const Surveys = ({ userType }) => {
   const { states, actions } = useSurvey(userType);
 
-  console.log('states',states);
-  // const [currentQuestion, setCurrentQuestion] = useState(0);
-  // const [showModal, setShowModal] = useState(false);
-  // const [isSurveyCompleted, setIsSurveyCompleted] = useState(false);
-  // const { alternativas, pregunta, preguntaNum } = questions[currentQuestion];
-
-  // const navigate = useNavigate();
-
-  // const handleSendAnswer = () => {
-  //   if (currentQuestion === questions.length - 1 && userType !== 'student') {
-  //     setShowModal(true);
-  //     return;
-  //   } else if (currentQuestion === questions.length - 1 && userType === 'student') {
-  //     //send answer
-  //     console.log('survey is completed from student');
-  //     setIsSurveyCompleted(true);
-  //     return;
-  //   }
-  //   currentQuestion < questions.length && setCurrentQuestion(currentQuestion + 1);
-  // };
-
-  // const handleFinishSurvey = () => {
-  //   // navigate('/completed-survey');
-  //   setIsSurveyCompleted(true);
-  // };
-
-  // const handlePreviousQuestion = () => {
-  //   currentQuestion > 0 && setCurrentQuestion(currentQuestion - 1);
-  // };
-
-  // const handleCloseModal = () => {
-  //   setShowModal(false);
-  // };
-
-  // if (isSurveyCompleted) return <CompletedSurvey type={userType} />;
   return (
     <section className="surveys">
       <SurveyContext.Provider value={{ userType, states, actions }}>
@@ -78,44 +43,6 @@ const Surveys = ({ userType }) => {
           )}
         </Loading>
       </SurveyContext.Provider>
-
-      {/* <article className="surveys__question-alternatives">
-        <div className="surveys__alternatives-container">
-          <span className="surveys__header-title">Encuesta {typeUser}</span>
-          <p className="surveys__question">{pregunta}</p>
-          <div className="alternatives__form" onChange={() => {}}>
-              <input className={userType} checked={questions[currentQuestion].alternativas.a.selected} id="option1" type="radio" name="option" value={questions[currentQuestion].alternativas.a.value} />
-              <input className={userType} checked={questions[currentQuestion].alternativas.b.selected} id="option2" type="radio" name="option" value={questions[currentQuestion].alternativas.b.value} />
-              <input className={userType} checked={questions[currentQuestion].alternativas.c.selected} id="option3" type="radio" name="option" value={questions[currentQuestion].alternativas.c.value} />
-              <input className={userType} checked={questions[currentQuestion].alternativas.d.selected} id="option4" type="radio" name="option" value={questions[currentQuestion].alternativas.d.value} />
-          </div>
-            <div className="form__input">
-              <input className={userType} id="option2" type="radio" name="option" value={alternativas.b} />
-              <label htmlFor="option2">{alternativas.b}</label>
-            </div>
-            <div className="form__input">
-              <input className={userType} id="option3" type="radio" name="option" value={alternativas.c} />
-              <label htmlFor="option3">{alternativas.c}</label>
-            </div>
-            <div className="form__input">
-              <input className={userType} id="option4" type="radio" name="option" value={alternativas.d} />
-              <label htmlFor="option4">{alternativas.d}</label>
-            </div>
-          <div className="form__actions">
-            <button
-              onClick={handlePreviousQuestion}
-              disabled={currentQuestion < 1}
-              className={`form__previous-question ${userType}`}
-            >
-              Pregunta anterior
-            </button>
-            <button onClick={handleSendAnswer} className={`form__next-question ${userType}`}>
-              {currentQuestion === questions.length - 1 ? 'Finalizar encuesta' : 'Continuar'}
-            </button>
-          </div>
-        </div>
-      </article>
-    */}
     </section>
   );
 };
