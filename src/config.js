e821b6fd381214dd6109823ac5245f85575c43e3
Sebastian Real <seba.real.a@gmail.com>
2023-03-14 19:36:20 -0300

    improvement: move the content code into config file

--- src/config.js
+++ src/config.js
@@ -13,12 +13,54 @@ const constants = {
   },
 };
 
+const links = {
+  needLink: {
+    description: '¿Necesitas el enlace de la encuesta de tus estudiantes nuevamente?',
+    textButton: 'Ver enlace encuesta',
+    textButtonColor: 'text-purple',
+    backgroundColor: 'background-purple',
+    width: 'container-width',
+    icon: '',
+  },
+  standardPlanification: {
+    description: 'Revisar planificación estandarizada.',
+    textButton: 'Ver planificación',
+    textButtonColor: 'text-red',
+    backgroundColor: 'background-gray',
+    width: 'container-width',
+    icon: '',
+  },
+  customPlanification: {
+    description: 'Revisar planificación personalizada.',
+    textButton: 'Ver planificación',
+    textButtonColor: 'text-purple',
+    backgroundColor: 'background-red',
+    width: 'container-width',
+    icon: '',
+  },
+};
+
+const emergentSituations = [
+  { title: 'Situaciones Emergentes' },
+  { title: 'Efemérides' },
+];
+
+const planningPrograms = [
+  { program: 'CiviConecta' },
+  { program: 'Ministerial' }
+];
+
 const config = {
   // baseURL: 'http://127.0.0.1:3001',
   baseURL: process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:3001',
   constants,
+  contents: {
+    links,
+    emergentSituations,
+    planningPrograms
+  }
 };
 
-console.log('config', config);
+// console.log('config', config);
 
 export default config;
