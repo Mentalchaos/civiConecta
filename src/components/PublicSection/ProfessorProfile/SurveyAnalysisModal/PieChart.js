a3931deba922c206b9243b99b39fe56414ebe2ab
Claudia Osses <claudia.osses.mundaca@gmail.com>
2023-03-20 20:01:53 -0300

    pie chart is added to analysis modal

---
+++ src/components/PublicSection/ProfessorProfile/SurveyAnalysisModal/PieChart.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import ApexCharts from 'apexcharts';
+
+class PieChart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.chartRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const options = {
+      series: [44, 55, 13, 43],
+      colors: ['#54bfed', '#c275ef', '#2dba9f', '#f48e76'],
+      chart: {
+      width: 330,
+      type: 'pie',
+      },
+      labels: ['Muy baja', 'Baja', 'Alta', 'Muy alta'],
+      dataLabels: {
+        style: {
+          fontSize: '12px',
+          textAlign: 'top',
+        },
+      },
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 250
+          },
+          legend: {
+            position: 'top'
+          }
+        }
+      }]
+      };
+
+    const chart = new ApexCharts(this.chartRef.current, options);
+    chart.render();
+  }
+
+  render() {
+    return (
+      <div ref={this.chartRef} />
+    );
+  }
+}
+
+export default PieChart;
