c4810a71def36ca21b68d233700f16fa4a98d03f
Jean Pierre Vera <jean.vera@opencasa.com>
2023-03-21 10:22:13 -0300

    removed student-survey from public

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,12 +4,14 @@ import NotFound from 'src/components/NotFound/NotFound';
 import AdminRouter from './AdminRouter';
 import AuthRouter from './AuthRouter';
 import PublicRouter from './PublicRouter';
+import StudentSurvey from 'src/components/PublicSection/Surveys/StudentSurvey';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<AuthRouter />} />
+        <Route exact path="/student-survey" element={<StudentSurvey />} />
         <Route path="/auth/*" element={<AuthRouter />} />
         <Route path="/admin/*" element={<AdminRouter />} />
         <Route path="/public/*" element={<PublicRouter />} />
