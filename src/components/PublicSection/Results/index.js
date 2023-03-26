845a33164ce95dbf1edd405f2035aabf46d3b0e0
Mentalchaos <jpierre22391@hotmail.com>
2023-03-26 07:41:23 -0300

    'Routes'

---
+++ src/components/PublicSection/Results/index.js
@@ -0,0 +1,13 @@
+import Footer from '../Footer/index';
+import './Results.css';
+
+const Results = () => {
+    return (
+        <div>
+            Results
+            <Footer />
+        </div>
+    )
+}
+
+export default Results;
