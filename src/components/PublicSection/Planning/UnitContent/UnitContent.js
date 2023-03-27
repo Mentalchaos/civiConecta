2d3839a085bd36542ee0db0286f206ccca5d3992
Jean Vera <jpierre22391@hotmail.com>
2023-03-27 15:14:34 -0300

    fixed footer margin

--- src/components/PublicSection/Planning/UnitContent/UnitContent.js
+++ src/components/PublicSection/Planning/UnitContent/UnitContent.js
@@ -2,7 +2,8 @@ import ButtonOptions from "../../UnitsDashboard/ButtonOptions";
 import back from 'src/assets/Icons/back-arrow.svg';
 import UnitTitle from "./UnitTitle";
 import './UnitContent.css'
-import UnitTable from "./UnitTable";
+import UnitTable from "./UnitTable"
+import Footer from "../../Footer";
 
 const UnitContent = () => {
     return (
