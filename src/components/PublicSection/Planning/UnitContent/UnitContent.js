65afcf6d41557fce9aef175f6a82a3bdfdfcfec9
ElBugs99 <jpvaldivia99@gmail.com>
2023-03-27 13:50:21 -0300

    Added planning page

---
+++ src/components/PublicSection/Planning/UnitContent/UnitContent.js
@@ -0,0 +1,23 @@
+import ButtonOptions from "../../UnitsDashboard/ButtonOptions";
+import back from 'src/assets/Icons/back-arrow.svg';
+import UnitTitle from "./UnitTitle";
+import './UnitContent.css'
+import UnitTable from "./UnitTable";
+
+const UnitContent = () => {
+    return (
+        <div className=''>
+            <div className="unit-content-container">
+                <ButtonOptions />
+                <button className='profile-back-container back1'>
+                    <img src={back} alt=''/>
+                    Volver
+                </button>
+                <UnitTitle />
+                <UnitTable />
+            </div>
+        </div>
+    )
+}
+
+export default UnitContent;
