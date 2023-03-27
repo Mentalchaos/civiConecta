890b35c2f776599cb5dbd98c763325b4ba7dfa1c
Jean Pierre Vera <jpierre22391@hotmail.com>
2023-03-26 21:42:22 -0300

    added more routes

---
+++ src/components/PublicSection/SituationsDashboard/index.js
@@ -0,0 +1,13 @@
+import Footer from '../Footer/index';
+import './SituationsDashboard.css';
+
+const SituationsDashboard = () => {
+    return (
+        <div>
+            SituationsDashboard
+            <Footer />
+        </div>
+    )
+}
+
+export default SituationsDashboard;
