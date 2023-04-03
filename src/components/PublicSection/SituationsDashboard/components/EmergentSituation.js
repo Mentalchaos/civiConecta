8d0986e3faac167b6de187a0c66e238dc5fdf840
Brian <brian_m_91@gmail.com>
2023-04-03 15:59:04 -0400

    fetch-and-map-ephemerides-emergen

--- src/components/PublicSection/SituationsDashboard/components/EmergentSituation.js
+++ src/components/PublicSection/SituationsDashboard/components/EmergentSituation.js
@@ -1,27 +1,26 @@
 import goTo from 'src/assets/Icons/open-arrow.svg';
 import '../SituationsDashboard.css';
+import '../index'
 
-const EmergentSituation = () => {
-    return (
-        <div className='situation-day'>
-            <div className='situation-date'>
-                <p>Clase Nº1</p>
-            </div>
-            <div className='situation-title'>
-                <p>Resolución de conflictos con los amigos</p>
-            </div>
-            <div className='situation-desc'>
-                <p>Comprender la amistad de manera sana (no
-                posesiva) y empática para lograr relaciones
-                de confianza y duraderas.
-                </p>
-            </div>
-            <div className='situation-see-class'>
-                <p>Ver clase</p>
-                <img src={goTo} alt='arrow-icon'/>
-            </div>
-        </div>
-    )
+const EmergentSituation = ({id,title, description}) => {
+  return (
+    <div className='situation-day'>
+      <div className='situation-date'>
+        <p> Clase Nº{id}</p>
+      </div>
+      <div className='situation-title'>
+        <p>{title}</p>
+      </div>
+      <div className='situation-desc'>
+        <p>{description}
+        </p>
+      </div>
+      <div className='situation-see-class'>
+        <p>Ver clase</p>
+        <img src={goTo} alt='arrow-icon' />
+      </div>
+    </div>
+  )
 }
 
 export default EmergentSituation;
--- src/components/PublicSection/SituationsDashboard/index.js
+++ src/components/PublicSection/SituationsDashboard/index.js
@@ -4,47 +4,73 @@ import Footer from '../Footer/index';
 import './SituationsDashboard.css';
 import EmergentSituation from './components/EmergentSituation';
 import SearchBar from './components/SearchBar';
+import config from 'src/config';
+import React, { useEffect, useState } from 'react';
+import { getUserData } from 'src/utils/user';
 
 const SituationsDashboard = () => {
-    return (
-        <div className='situations-section'>
-            <div className="situations">
-                <div className='situations-cont'>
-                    <div className='situations-header'>
-                        <img className='situations-image' src={brain} alt='logo'></img>
-                        <div className='situations-header-text'>
-                            <p>Situaciones emergentes</p>
-                        </div>
-                    </div>
-                    <div className='situations-description-container'>
-                        <div className='situations-description'>
-                            <img className='book-icon' src={unitGreen} alt='situations-icon' />
-                            <div className='situations-info'>
-                                <div className='situations-desc-title'>Descripción:</div>
-                                <div className='situations-desc-text'>Clases para profundizar y complementar temáticas ya abordadas en unidades anteriores,
-                                además de posibles conflictos que se dan en el aula. Utiliza el buscador para encontrar clases
-                                atingentes con las temáticas que necesites.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+  const [emergentData, setEmergentData] = useState([]);
+
+  const getSituations = async () => {
+    const baseUrl = `${config.baseURL}/events/1`;
+    const response = await fetch(baseUrl, {
+      headers: {
+        token: getUserData().token,
+        "Content-Type": "application/json"
+      },
+      method: 'GET'
+    })
+    const data = await response.json();
+    setEmergentData(data);
+  };
+  useEffect(() => {
+    getSituations();
+  }, []);
+
+  return (
+    <div className='situations-section'>
+      <div className="situations">
+        <div className='situations-cont'>
+          <div className='situations-header'>
+            <img className='situations-image' src={brain} alt='logo'></img>
+            <div className='situations-header-text'>
+              <p>Situaciones emergentes</p>
+            </div>
+          </div>
+          <div className='situations-description-container'>
+            <div className='situations-description'>
+              <img className='book-icon' src={unitGreen} alt='situations-icon' />
+              <div className='situations-info'>
+                <div className='situations-desc-title'>Descripción:</div>
+                <div className='situations-desc-text'>Clases para profundizar y complementar temáticas ya abordadas en unidades anteriores,
+                  además de posibles conflictos que se dan en el aula. Utiliza el buscador para encontrar clases
+                  atingentes con las temáticas que necesites.
                 </div>
+              </div>
             </div>
-            <div className='situations-search'>
-                <SearchBar />
-            </div>
-            <div className="classes-container">
-                <div className="classes-section">
-                    <EmergentSituation />
-                    <EmergentSituation />
-                    <EmergentSituation />
-                    <EmergentSituation />
-                    <EmergentSituation />
-                </div>
-            </div>
-            <Footer />
+          </div>
         </div>
-    )
+      </div>
+      <div className='situations-search'>
+        <SearchBar />
+      </div>
+      <div className="classes-container">
+        <div className="classes-section">
+          {emergentData.events && emergentData.events.map((data) => (
+            <EmergentSituation
+              key={data.id}
+              id={data.id}
+              title={data.title}
+              description={data.description}
+              date={data.date}
+            />
+          ))};
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default SituationsDashboard;
