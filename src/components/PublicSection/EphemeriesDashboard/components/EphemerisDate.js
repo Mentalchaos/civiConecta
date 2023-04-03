7f73ad9a4be8e0a9345bcb74d5d72157936ba874
Brian <brian_m_91@gmail.com>
2023-04-03 15:59:04 -0400

    fetch-and-map-ephemerides-emergen

--- src/components/PublicSection/EphemeriesDashboard/components/EphemerisDate.js
+++ src/components/PublicSection/EphemeriesDashboard/components/EphemerisDate.js
@@ -1,14 +1,14 @@
 import goTo from 'src/assets/Icons/open-arrow.svg';
 import '../EphemeriesDashboard.css';
 
-const EphemerisDate = () => {
+const EphemerisDate = ({id, title, description, date}) => {
     return (
         <div className='ephemeris-day'>
             <div className='ephemeris-date'>
-                <p>08/03</p>
+                <p>{date}</p>
             </div>
             <div className='ephemeris-title'>
-                <p>Día internacional de la mujer.</p>
+                <p>{title}.</p>
             </div>
             <div className='ephemeris-see-class'>
                 <p>Ver clase</p>
