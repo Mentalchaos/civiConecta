54af38dff090328aa745cdf67b7ae9d9b42b1fa3
Jean Pierre Vera <jean.vera@opencasa.com>
2023-04-09 10:40:53 -0400

    Commented code for watch reports, cuz we dont have the data yet

--- src/components/PublicSection/ProfessorProfile/ProfessorInfo.js
+++ src/components/PublicSection/ProfessorProfile/ProfessorInfo.js
@@ -1,16 +1,14 @@
 import { useState, useEffect } from 'react';
-import './DisguisedInput.js';
+import { getUserData } from 'src/utils/user';
 import config from 'src/config';
 import DisguisedInput from './DisguisedInput.js';
+import ModalTrigger from './ModalTrigger';
+
 import MenuDocenteIcon from 'src/assets/Icons/menu-docente.svg';
 import profile from 'src/assets/Icons/profile-image.svg';
 import finishImage from 'src/assets/images/finish-survey.png';
 import report from 'src/assets/images/report-container.png';
-import ModalTrigger from './ModalTrigger';
-import { getUserData } from 'src/utils/user';
-
 import './professor-profile.css';
-
 
 const ProfessorInfo = ({ onClick }) => {
   const [userData, setUserData] = useState({});
@@ -24,18 +22,18 @@ const ProfessorInfo = ({ onClick }) => {
         token: userData.token
       }
     })
-    .then(d => d.json())
-    .then(data => setUserData(data.info))
+      .then(d => d.json())
+      .then(data => setUserData(data.info))
   }, [])
 
   const data = [
     {
       label: "Email",
       value: currentUser.email
-    },{
+    }, {
       label: "Institución",
       value: userData.establishment
-    },{
+    }, {
       label: "Curso",
       value: userData.grade
     }
@@ -45,33 +43,37 @@ const ProfessorInfo = ({ onClick }) => {
 
   return (
     <div className="professor-info-container">
-        <div className='professor-info-title'>
-          <img className='professor-profile-img' src={profile} alt="professor-profile-img"/>
-          <p>Perfil</p>
+      <div className='professor-info-title'>
+        <img className='professor-profile-img' src={profile} alt="professor-profile-img" />
+        <p>Perfil</p>
+      </div>
+      <div className="professor-info-data">
+        <div className="professor-info-data-cont">
+          <img src={MenuDocenteIcon} alt="menu-icon" />
+          <p className="professor-info-name">{currentUser.name}</p>
+          <p className="professor-info-type">Docente</p>
         </div>
-        <div className="professor-info-data">
-          <div className="professor-info-data-cont">
-            <img src={MenuDocenteIcon} alt="menu-icon" />
-            <p className="professor-info-name">{ currentUser.name }</p>
-            <p className="professor-info-type">Docente</p>
-          </div>
-          <div className="disguised-inputs-cont">
-            { inputs }
-          </div>
+        <div className="disguised-inputs-cont">
+          {inputs}
         </div>
-        <ModalTrigger
-          onClick={onClick}
-          img={finishImage}
-          title={'¿Deseas terminar la encuesta?'}
-          buttonText={'Finalizar encuesta'}
-        />
+      </div>
+      <ModalTrigger
+        onClick={onClick}
+        img={finishImage}
+        title={'¿Deseas terminar la encuesta?'}
+        buttonText={'Finalizar encuesta'}
+      />
+      {/*
+        @TODO: This should be activated after the button about finishing the encuesta was clicked
         <ModalTrigger
           img={report}
           title={'Informe de resultados ¡Ya disponible!'}
           buttonText={'Ver reporte'}
-        />
+      /> */}
     </div>
   )
 }
+
+ProfessorInfo.displayName = 'ProfessorInfo';
 
 export default ProfessorInfo;
