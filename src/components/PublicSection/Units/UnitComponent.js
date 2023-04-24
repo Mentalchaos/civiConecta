4cd0ae0c518a62c733d216764a16929e927f4480
Brian Muñoz <brian.progra.91@gmail.com>
2023-04-24 17:53:46 -0400

    unit-correction

--- src/components/PublicSection/Units/UnitComponent.js
+++ src/components/PublicSection/Units/UnitComponent.js
@@ -3,34 +3,34 @@ import brain from 'src/assets/Icons/heart-brain.svg';
 import goTo from 'src/assets/Icons/open-arrow.svg';
 import './UnitComponent.css';
 
-const UnitComponent = ({status, title, subtitle, description, color, borderColor, number}) => {
-    return (
-        <div className={`unit-component-container ${color}`}>
-            <div className='unit-component-title'>
-                <div>
-                    <img src={unitLogo} alt='unit-logo' />
-                </div>
-                <div className='unit-second-container'>
-                    <p>{status}</p>
-                    <img src={brain} alt='brain-logo'/>
-                </div>
-            </div>
-            <div className='component-info'>
-            <p className='component-title'>Unidad {number}</p>
-                <p className={`component-subtitle ${borderColor}`}>{title}</p>
-                <p className='component-description'>{description}</p>
-            </div>
-            <div className='go-to-unit'>
-                <p>Ir a la unidad</p>
-                <img src={goTo} alt="arrow-icon" />
-            </div>
+const UnitComponent = ({ status, title, subtitle, description, color, borderColor, number }) => {
+  return (
+    <div className={`unit-component-container ${color}`}>
+      <div className='unit-component-title'>
+        <div>
+          <img src={unitLogo} alt='unit-logo' />
         </div>
-    )
+        <div className='unit-second-container'>
+          <p>{status}</p>
+          <img src={brain} alt='brain-logo' />
+        </div>
+      </div>
+      <div className='component-info'> 
+        <p className='component-title'>Unidad {number} </p>
+        <p className={`component-subtitle ${borderColor}`}>{title}</p>
+        <p className='component-description'>{description}</p>
+      </div>
+      <div className='go-to-unit'>
+        <p>Ir a la unidad</p>
+        <img src={goTo} alt="arrow-icon" />
+      </div>
+    </div>
+  )
 }
 
 UnitComponent.defaultProps = {
-    color: "unit-purple",
-    borderColor: "border-purple"
-  };
+  color: "unit-gray",
+  borderColor: "border-purple"
+};
 
 export default UnitComponent;
