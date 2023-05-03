f8fb811334034c3063cd5de7d71fc3e9877b2a9f
Brian Muñoz <brian.progra.91@gmail.com>
2023-05-03 16:47:07 -0400

    api-data-binding

--- src/components/PublicSection/ProfessorProfile/Answer.js
+++ src/components/PublicSection/ProfessorProfile/Answer.js
@@ -1,20 +1,20 @@
 import arrow from 'src/assets/Icons/open-arrow.svg';
 
-const Answer = ({question, status, onClick}) => {
+const Answer = ({ answer, onClick }) => {
   return (
     <div className="answer-container">
       <div>
-        <p>{question}</p>
+        <p>{answer}</p>
       </div>
       <div className="answer-container-alert">
         <div className="answer-alert-icon">
           <p>!</p>
         </div>
-        <p className='answer-status'>{status}</p>
+        <p className='answer-status'>Requiere atención</p>
       </div>
       <div className='answer-status-button'>
-        <button onClick={onClick}> 
-          Ver más <img src={arrow}/> 
+        <button onClick={onClick}>
+          Ver más <img src={arrow} alt='img'/>
         </button>
       </div>
     </div>
--- src/components/PublicSection/ProfessorProfile/StudentNomina.js
+++ src/components/PublicSection/ProfessorProfile/StudentNomina.js
@@ -1,44 +1,59 @@
-import './professor-profile.css';
+import { useEffect, useState } from 'react';
 import StudentTableBody from './StudentTableBody.js';
-import listIcon from 'src/assets/Icons/student-list.svg';
 import CriticalAnswers from './CriticalAnswers';
+import { getUserData } from 'src/utils/user';
+import config from 'src/config';
+import listIcon from 'src/assets/Icons/student-list.svg';
+import './professor-profile.css';
+
 
 const values = ["Alumno", "Nombre", "Rut", "Encuesta", "Fecha"];
-const tbodyValues = ["01","Eduardo Cossio Brante", "24.040.904-9","Pendiente", "24/08/22"];
+const tbodyValues = ["01", "Eduardo Cossio Brante", "24.040.904-9", "Pendiente", "24/08/22"];
 
-const StudentNomina = ({onClick}) => {
+const StudentNomina = ({ onClick }) => {
+
+  const [studentData, setStudentData] = useState([]);
+
+  const getStudentNomina = async () => {
+
+    const userData = getUserData();
+    const baseURL = `${config.baseURL}/reports/student-completion/${userData.uuid}`;
+
+    const response = await fetch(baseURL, {
+      headers: {
+        token: getUserData().token,
+        "Content-Type": "application/json"
+      },
+      method: "GET"
+    })
+    const data = await response.json();
+    setStudentData(data.report)
+
+  };
+  useEffect(() => {
+    getStudentNomina();
+  }, []);
+
   return (
     <div className="student-nomina-container">
-      <CriticalAnswers onClick={onClick}/>
+      <CriticalAnswers onClick={onClick} />
       <div className='student-nomina-title'>
-        <img className='student-nomina-icon' src={listIcon} />
+        <img className='student-nomina-icon' src={listIcon} alt='img' />
         <p>Nómina</p>
       </div>
       <div className="student-nomina-table-container">
         <div className="student-tablehead">
-          { values.map(data => <p key={data} className="student-tablehead-values">{data}</p>)}
+          {values.map(data => <p key={data} className="student-tablehead-values">{data}</p>)}
         </div>
         <div className="tbody-cont">
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-          <StudentTableBody value={tbodyValues}/>
-
-          <div className="student-tablebody">
-            { tbodyValues.map(data => <p key={data} className="student-tablebody-values">{data}</p>)}
-          </div>
-          <div className="student-tablebody">
-            { tbodyValues.map(data => <p key={data} className="student-tablebody-values">{data}</p>)}
-          </div>
+          {
+            studentData && studentData.map((data, key) =>
+              <StudentTableBody
+                key={data.run}
+                id={key + 1}
+                data={data}
+              />
+            )}
         </div>
       </div>
     </div>
