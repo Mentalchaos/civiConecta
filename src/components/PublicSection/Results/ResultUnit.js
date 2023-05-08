2a35476c00caf3c7645245a9ad40c0ac8fff72f2
Jean Pierre Vera <jean.vera@opencasa.com>
2023-05-07 21:11:01 -0400

    Added more elements to the /result section

--- src/components/PublicSection/Results/ResultUnit.js
+++ src/components/PublicSection/Results/ResultUnit.js
@@ -8,6 +8,11 @@ const ResultUnit = ({ id, data }) => {
     <div className='graphic_content'>
       { id == 0 && <p className='graphic_title'>Unidad 1 - Relaciones Interpersonales</p>}
       <p className='graphic_text'>
+        {/*
+          En vez de este texto de aca aqui deberia
+          estar el titulo que se encuentra en el elemento 'question'
+          Actualmente ese texto en cada vuelta nos muestra 'pregunta tres' y 'pregunta cuatro'
+        */}
         1. ¿Cómo calificarías tu capacidad para reconocer tus cualidades y habilidades,
         por ejemplo: honestidad, respeto, sensibilidad, responsabilidad, solidaridad,
         comunicación, motivación, paciencia, trabajo en equipo, etc.?
