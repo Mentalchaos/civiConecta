f1a6b24d0fa14fc4562b951c794a34469705331d
Brian Muñoz <brian.progra.91@gmail.com>
2023-05-08 16:44:14 -0400

    created-pie-chart-for-result-section

--- src/components/PublicSection/ProfessorProfile/SurveyAnalysisModal/ResultPieChart.js
+++ src/components/PublicSection/ProfessorProfile/SurveyAnalysisModal/ResultPieChart.js
@@ -2,22 +2,19 @@ import React, { useEffect, useRef } from 'react';
 import ApexCharts from 'apexcharts';
 
 const ResultPieChart = ({ series, labels }) => {
+
   const chartRef = useRef(null);
 
   useEffect(() => {
-    // const series = [];
-    // const labels = [];
-    // dataPieChart.map(data => series.push(data.percentage));
-    // dataPieChart.map(data => labels.push(data.label));
 
     const options = {
-      series: [50,30,20],
+      series: series,
       colors: ['#54bfed', '#c275ef', '#2dba9f', '#f48e76'],
       chart: {
         width: 360,
         type: 'pie',
       },
-      labels: ['Label 1', 'Label 2', 'Label 3'],
+      labels: labels,
       dataLabels: {
         style: {
           fontSize: '12px',
--- src/components/PublicSection/Results/ResultUnit.js
+++ src/components/PublicSection/Results/ResultUnit.js
@@ -1,24 +1,24 @@
 import ResultPieChart from 'src/components/PublicSection/ProfessorProfile/SurveyAnalysisModal/ResultPieChart.js'
 
-const ResultUnit = ({ id, data }) => {
-  console.log('id', id);
+const ResultUnit = ({ id, question, pieChartData }) => {
 
+  const label = [];
+  const series = [];
+
+  pieChartData.map(data => label.push(data.label));
+  pieChartData.map(data => series.push(data.percentage));
 
   return (
     <div className='graphic_content'>
-      { id == 0 && <p className='graphic_title'>Unidad 1 - Relaciones Interpersonales</p>}
+      {id === 0 && <p className='graphic_title'>Unidad 1 - Relaciones Interpersonales</p>}
       <p className='graphic_text'>
-        {/*
-          En vez de este texto de aca aqui deberia
-          estar el titulo que se encuentra en el elemento 'question'
-          Actualmente ese texto en cada vuelta nos muestra 'pregunta tres' y 'pregunta cuatro'
-        */}
-        1. ¿Cómo calificarías tu capacidad para reconocer tus cualidades y habilidades,
-        por ejemplo: honestidad, respeto, sensibilidad, responsabilidad, solidaridad,
-        comunicación, motivación, paciencia, trabajo en equipo, etc.?
+        {question}
       </p>
       <div className='graphic'>
-        <ResultPieChart />
+        <ResultPieChart
+          labels={label}
+          series={series}
+        />
       </div>
     </div>
   )
