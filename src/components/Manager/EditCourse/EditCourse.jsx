bb59aa999a964637b32941ffa1f170fa973ff8f0
Sebastian Real <seba.real.a@gmail.com>
2023-09-30 19:14:57 -0300

    por mientras

--- src/components/Manager/EditCourse/EditCourse.jsx
+++ src/components/Manager/EditCourse/EditCourse.jsx
@@ -9,31 +9,39 @@ const EditCourse = () => {
   return (
     <>
       <EditCourseContext.Provider value={{ states, actions }}>
-        <CreateStudentForm />
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Nombre</th>
-                <th>run</th>
-                <th>curso</th>
-                <th>letra</th>
-              </tr>
-            </thead>
-            <tbody>
-              {states.students.map(student => {
-                return (
-                  <tr key={student.id}>
-                    <th>{student.name}</th>
-                    <th>{student.run}</th>
-                    <th>{states.course.level}</th>
-                    <th>{states.course.character}</th>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <fieldset>
+          <legend>Seccion estudiantes</legend>
+          <CreateStudentForm />
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>run</th>
+                  <th>curso</th>
+                  <th>letra</th>
+                </tr>
+              </thead>
+              <tbody>
+                {states.students.map(student => {
+                  return (
+                    <tr key={student.id}>
+                      <th>{student.name}</th>
+                      <th>{student.run}</th>
+                      <th>{states.course.level}</th>
+                      <th>{states.course.character}</th>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Seccion docentes</legend>
+          <div>seccion crear profesor</div>
+          <div>lista de profesores</div>
+        </fieldset>
       </EditCourseContext.Provider>
     </>
   );
