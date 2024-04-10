36dfc570085e966c16bcbc70d6a262fb2e75eced
JP Vera <jpierre22391@hotmail.com>
2024-04-10 10:02:27 -0400

    Removed unused description

--- src/components/PublicSection/UnitsDashboard/UnitClass.jsx
+++ src/components/PublicSection/UnitsDashboard/UnitClass.jsx
@@ -4,13 +4,13 @@ import { useNavigate } from 'react-router-dom';
 const UnitClass = ({ id, objective, description, number }) => {
   const navigate = useNavigate();
 
-  const hasDescription = description == 'Sin descripcion' ? '' : description;
+  // const hasDescription = description == 'Sin descripcion' ? '' : description;
 
   return (
     <div className='class'>
       <div className='class-title'>Clase Nº{number}</div>
       <div className='sub-title'>{ objective }</div>
-      <div className='class-text'>{ hasDescription }</div>
+      {/* <div className='class-text'>{ hasDescription } eaea</div> */}
       <div className='see-class-container'>
         <button className='see-class' onClick={() => navigate(`/public/planning/${id}/${objective}/unit`)}>
           Ver clase
